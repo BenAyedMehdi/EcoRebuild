6fe5cfc5d815eab4c78c5d6e730ff4bc0ecd6c5b
BenAyedMehdi <mehdibenayed97@gmail.com>
2025-03-20 23:09:35 +0100

    last quick formatting fix

--- src/newComponents/goals-and-values.component/goals-and-values.component.jsx
+++ src/newComponents/goals-and-values.component/goals-and-values.component.jsx
@@ -10,73 +10,89 @@ const GoalsAndValuesComponent = () => {
           <div className="flex flex-1 flex-col gap-10 items-center justify-center w-full">
             <div className="flex flex-col gap-5 items-start justify-start w-full">
               <Text
-                  className="text-center text-4xl sm:text-[32px] md:text-[34px] text-gray-900 tracking-[-0.72px] w-full"
-                  size="txtManropeExtraBold36"
+                className="text-center text-4xl sm:text-[32px] md:text-[34px] text-gray-900 tracking-[-0.72px] w-full"
+                size="txtManropeExtraBold36"
               >
                 Goals and Values
               </Text>
-              <Text className="text-gray-700 mt-3" size="txtManropeRegular18Gray700">
+              {/* <Text
+                className="text-gray-700 mt-3"
+                size="txtManropeRegular18Gray700"
+              >
                 EcoRebuild work is defined by key values:
-              </Text>
+              </Text> */}
             </div>
             <List
-                className="flex flex-col gap-8 items-center w-full"
-                orientation="vertical"
+              className="flex flex-col gap-8 items-center w-full"
+              orientation="vertical"
             >
               <div className="text-center flex flex-1 sm:flex-col flex-row gap-[26px] items-start justify-start my-0 w-full">
                 <div className="flex flex-1 flex-col gap-[17px] items-start justify-start w-full">
                   <Text
-                      className="text-[20px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
-                      size="txtManropeBold22"
+                    className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
+                    size="txtManropeBold22"
                   >
-                    <strong>• Empowerment:</strong> Equipping communities to take ownership of rebuilding their environment.
+                    <strong>• Empowerment:</strong>
                   </Text>
                   <Text
-                      className="text-gray-700"
-                      size="txtManropeRegular18Gray700"
-                  ></Text>
+                    className="text-gray-700"
+                    size="txtManropeRegular18Gray700"
+                  >
+                    {" "}
+                    Equipping communities to take ownership of rebuilding their
+                    environment.
+                  </Text>
                 </div>
               </div>
               <div className="flex flex-1 sm:flex-col flex-row gap-[26px] items-start justify-start my-0 w-full">
                 <div className="flex flex-1 flex-col gap-[17px] items-start justify-start w-full">
                   <Text
-                      className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
-                      size="txtManropeBold22"
+                    className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
+                    size="txtManropeBold22"
                   >
-                    <strong>• Sustainability:</strong> Developing long-lasting, eco-friendly solutions.
+                    <strong>• Sustainability:</strong>
                   </Text>
                   <Text
-                      className="text-gray-700"
-                      size="txtManropeRegular18Gray700"
-                  ></Text>
+                    className="text-gray-700"
+                    size="txtManropeRegular18Gray700"
+                  >
+                    {" "}
+                    Developing long-lasting, eco-friendly solutions.
+                  </Text>
                 </div>
               </div>
               <div className="flex flex-1 sm:flex-col flex-row gap-[26px] items-start justify-start my-0 w-full">
                 <div className="flex flex-1 flex-col gap-[17px] items-start justify-start w-full">
                   <Text
-                      className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
-                      size="txtManropeBold22"
+                    className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
+                    size="txtManropeBold22"
                   >
-                  <strong>• Solidarity:</strong> Standing by conflict-affected regions, starting with Gaza, to support renewal and justice.
+                    <strong>• Solidarity:</strong>
                   </Text>
                   <Text
-                      className="text-gray-700"
-                      size="txtManropeRegular18Gray700"
-                  ></Text>
+                    className="text-gray-700"
+                    size="txtManropeRegular18Gray700"
+                  >
+                    Standing by conflict-affected regions, starting with Gaza,
+                    to support renewal and justice.
+                  </Text>
                 </div>
               </div>
               <div className="flex flex-1 sm:flex-col flex-row gap-[26px] items-start justify-start my-0 w-full">
                 <div className="flex flex-1 flex-col gap-[17px] items-start justify-start w-full">
                   <Text
-                      className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
-                      size="txtManropeBold22"
+                    className="text-[22px] text-gray-900 sm:text-lg md:text-xl tracking-[-0.44px] w-full"
+                    size="txtManropeBold22"
                   >
-                    <strong>• Frugal Innovation:</strong> Making impactful change possible using the simplest, most accessible resources.
+                    <strong>• Frugal Innovation:</strong>
                   </Text>
                   <Text
-                      className="text-gray-700"
-                      size="txtManropeRegular18Gray700"
-                  ></Text>
+                    className="text-gray-700"
+                    size="txtManropeRegular18Gray700"
+                  >
+                    Making impactful change possible using the simplest, most
+                    accessible resources.
+                  </Text>
                 </div>
               </div>
             </List>
